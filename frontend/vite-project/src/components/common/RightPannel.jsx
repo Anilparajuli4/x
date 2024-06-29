0fcefa71c1eb28ca88017a0a201243f799786658
Anil Parajuli <anilparajuli998@gmail.com>
2024-06-29 10:43:49 +0545

    follow api fetching

--- frontend/vite-project/src/components/common/RightPannel.jsx
+++ frontend/vite-project/src/components/common/RightPannel.jsx
@@ -1,9 +1,28 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
+import useFollow from "../../hook/useFollow";
 
 const RightPanel = () => {
-	const isLoading = false;
+	const {data:suggestedUser, isLoading} = useQuery({
+     queryKey:['suggestedUsers'],
+ queryFn: async()=>{
+		try {
+			const res = await fetch('/api/users/suggested')
+			const data = await res.json()
+			if(!res.ok){
+				throw new Error(data.error || 'somethong went wrong')
+			}
+			return data
+		} catch (error) {
+			throw new Error(error)
+		}
+	
+ }
+	})
+
+	const {follow, isPending} = useFollow()
 
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
@@ -20,7 +39,7 @@ const RightPanel = () => {
 						</>
 					)}
 					{!isLoading &&
-						USERS_FOR_RIGHT_PANEL?.map((user) => (
+						suggestedUser?.data?.map((user) => (
 							<Link
 								to={`/profile/${user.username}`}
 								className='flex items-center justify-between gap-4'
@@ -34,7 +53,7 @@ const RightPanel = () => {
 									</div>
 									<div className='flex flex-col'>
 										<span className='font-semibold tracking-tight truncate w-28'>
-											{user.fullName}
+											{user.fullname}
 										</span>
 										<span className='text-sm text-slate-500'>@{user.username}</span>
 									</div>
@@ -42,9 +61,12 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
+										onClick={(e) => {e.preventDefault()
+											follow(user._id)
+										}
+										}
 									>
-										Follow
+										{isPending ? 'loading' : 'follow'}
 									</button>
 								</div>
 							</Link>
